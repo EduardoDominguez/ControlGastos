6c22a53039791ad31476e769f9244d3370ce4351
Mario Eduardo Domínguez Meléndez <mario.ed.domm@gmail.com>
2019-05-07 15:31:25 -0500

    Con notificaciones

--- app/src/main/java/mx/univa/controldegastos/PrincipalMain.kt
+++ app/src/main/java/mx/univa/controldegastos/PrincipalMain.kt
@@ -7,6 +7,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.NavigationView
 import android.support.v4.app.Fragment
+import android.support.v4.app.NotificationCompat
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
@@ -23,6 +24,8 @@ class PrincipalMain : AppCompatActivity(), NavigationView.OnNavigationItemSelect
     private var txtUserHeader: TextView? = null
     private var txtCorreoHeader: TextView? = null
     private var navBar: NavigationView? = null
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
@@ -130,14 +133,14 @@ class PrincipalMain : AppCompatActivity(), NavigationView.OnNavigationItemSelect
             var nombre_completo:String = ""
             var id_usuario : Int = 0
 
-            if(bundle.get("correo").toString() != null)
+            if(!bundle.get("correo").toString().isNullOrEmpty())
                 correo = bundle.get("correo").toString()
 
-            if(bundle.get("nombre_completo").toString() != null)
+            if(!bundle.get("nombre_completo").toString().isNullOrEmpty())
                 nombre_completo = bundle.get("nombre_completo").toString()
 
 
-            if(bundle.get("id_usuario").toString() != null)
+            if(!bundle.get("id_usuario").toString().isNullOrEmpty())
                 id_usuario = Integer.parseInt(bundle.get("id_usuario").toString())
 
 
